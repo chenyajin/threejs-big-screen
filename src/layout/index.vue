43482e57ccb1820912b41de1abaec866cec375f5
陈亚金 <990189841@qq.com>
2024-07-17 16:05:30 +0800

    fix: 添加模型

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,7 +2,7 @@
   <div class="layout">
     <Header>数据中心可视化系统</Header>
     <div class="layout-content">
-      <div class="layout-content-left">
+      <!-- <div class="layout-content-left">
         <Panel>
           <div>向西28° 2023/04/02 12:00 正常</div>
           <div>向西28° 2023/04/02 12:00 正常</div>
@@ -11,23 +11,23 @@
         <Panel>
           <PieChart></PieChart>
         </Panel>
-      </div>
-      <div class="layout-content-right">
+      </div> -->
+      <!-- <div class="layout-content-right">
         <Panel>
           <LineCharts></LineCharts>
         </Panel>
-      </div>
+      </div> -->
       <Sence></Sence>
     </div>
   </div>
 </template>
 
 <script setup lang="ts" name="Layout">
-import Header from './Header.vue';
-import Sence from '@/components/Sence.vue';
-import Panel from '@/components/Panel/index.vue';
-import LineCharts from '@/components/Charts/LineChart.vue';
-import PieChart from '@/components/Charts/PieChart.vue';
+import Header from './Header.vue'
+import Sence from '@/components/Sence.vue'
+import Panel from '@/components/Panel/index.vue'
+import LineCharts from '@/components/Charts/LineChart.vue'
+import PieChart from '@/components/Charts/PieChart.vue'
 </script>
 
 <style scope>
